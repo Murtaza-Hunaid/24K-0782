3e71bc2a7d15bd379bcd5933f9172c60a808f351
MurtazaHunaid <k240782@nu.edu.pk>
2024-09-18 17:56:13 +0500

    Update Question#1.c

--- Lab 4/Question#1.c
+++ Lab 4/Question#1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-  int num;
+	int num;
 	printf("Enter a number: ");
 	scanf("%d", &num);
 	if (num%3 == 0){
-	  printf("%d is a multiple of 3", num);} 
+		printf("%d is a multiple of 3", num);} 
 	else {
-	  printf("%d is not a multiple of 3...", num);}
+		printf("%d is not a multiple of 3...", num);}
 }

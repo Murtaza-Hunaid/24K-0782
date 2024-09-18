f84a3778039a5461a767e7875170e186d7f0e176
MurtazaHunaid <k240782@nu.edu.pk>
2024-09-18 18:02:30 +0500

    Create Question#3.c

---
+++ Lab 4/Question#3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+  float num1, num2;
+	char operator;
+	
+	printf("Enter in this format: Number Operator Number\n");
+	scanf("%f %c %f", &num1, &operator, &num2);
+	
+	switch(operator){
+		case '+':
+			printf("%.2f + %.2f = %.2f", num1, num2, num1 + num2);
+			break;
+		case '-':
+			printf("%.2f - %.2f = %.2f", num1, num2, num1 - num2);
+			break;
+		case '*':
+			printf("%.2f * %.2f = %.2f", num1, num2, num1 * num2);
+			break;
+		case '/':
+			printf("%.2f / %.2f = %.2f", num1, num2, num1 / num2);
+			break;
+		default:
+			printf("invalid Format entered");}
+	}

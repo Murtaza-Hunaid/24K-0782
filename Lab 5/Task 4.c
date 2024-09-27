62119c02664dc839685ab8cf1df4a144cd7efac8
MurtazaHunaid <k240782@nu.edu.pk>
2024-09-28 02:20:53 +0500

    Update Task 4.c

--- Lab 5/Task 4.c
+++ Lab 5/Task 4.c
@@ -4,19 +4,16 @@ int main()
  int ph;
  printf("Enter the pH value: ");
  scanf("%d", &ph);
- 
- if (ph > 7)
-    {if (ph < 12)
+ if (ph > 7){
+     if (ph < 12)
         {printf("Alkaline");}
      else
-        {printf("Very Alkaline");}
-    }
- else if (ph == 7)
-    {printf("Neutral");}
- else if (ph > 2)
-    {printf("Acidic");}
- else
-    {printf("Very Acidic");}
-
+        {printf("Very Alkaline");}}
+ else if (ph == 7){
+     printf("Neutral");}
+ else if (ph > 2){
+     printf("Acidic");}
+ else{
+     printf("Very Acidic");}
  return 0;
 }

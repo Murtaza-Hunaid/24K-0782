e888949534ed9f43bc8ac1c48c9409cc75b909d4
MurtazaHunaid <k240782@nu.edu.pk>
2024-09-28 02:36:05 +0500

    Update Task 7.c

--- Lab 5/Task 7.c
+++ Lab 5/Task 7.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main(){
 	int num;
-	
 	printf("Enter Number: ");
 	scanf("%d",&num);
-	
-	(num%10==0)?printf("Last Digit is Zero."):printf("Last Digit is Non-Zero.");
-		
+	(num%10==0) ? printf("Last Digit is Zero") : printf("Last Digit is Non-Zero");
 }

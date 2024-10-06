34b3ff36a680d03ac1787ff0a0cc34d695557c91
MurtazaHunaid <k240782@nu.edu.pk>
2024-10-06 16:51:42 +0500

    Update Question 7.c

--- Lab 6/Question 7.c
+++ Lab 6/Question 7.c
@@ -1,3 +1,9 @@
 #include<stdio.h>
-int main(){
-  
+int main(){ 
+  char i; 
+  for(i = 'A' ; i <= 'F' ; i ++){
+     if(i == 'A' || i == 'F'){
+        printf("%c %c %c %c %c\n", i, i+1, i+2, i+3, i+4);}
+     else {
+        printf("%c       %c\n", i, i+4);}}
+}

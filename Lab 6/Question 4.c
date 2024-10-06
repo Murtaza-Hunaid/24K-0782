183459f3c6d686d940ac71f08661e08a9b295ae4
MurtazaHunaid <k240782@nu.edu.pk>
2024-10-06 16:49:11 +0500

    Update Question 4.c

--- Lab 6/Question 4.c
+++ Lab 6/Question 4.c
@@ -1,19 +1,15 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 int main(){
-	int num, digit=0, sum=0, exp = 3;
-	
-	printf("Enter Number to check if its ARMSTRONG or NOT: ");
-	scanf("%d",&num);
-	int temp = num;
-	while(temp>0){
-		digit = temp%10;
-		sum += pow(digit, exp);
-		temp/=10;
-	}
-	if (sum == num)
-		printf("Number is ARMSTRONG...");
-	else
-		printf("Number is not ARMSTRONG...");
-	
+	int num, sum = 0, temp, digit;
+   	printf("Enter a number: ");
+    	scanf("%d", &num);
+    	temp = num;
+    	while (temp != 0){
+        	digit = temp % 10;
+        	sum = sum + (digit * digit * digit);
+        	temp = temp / 10;}
+   	if (sum == num){
+        	printf("%d is an Armstrong number\n", num);}
+    	else {
+        	printf("%d is not an Armstrong number\n", num);}
 }

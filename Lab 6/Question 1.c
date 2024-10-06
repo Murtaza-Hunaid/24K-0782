6a46e5f3e663a13d21978a9cacb5b2716c6186f7
MurtazaHunaid <k240782@nu.edu.pk>
2024-10-06 17:10:10 +0500

    Update Question 1.c

--- Lab 6/Question 1.c
+++ Lab 6/Question 1.c
@@ -1,1 +1,15 @@
+/* Do while is the suitable option as firstly, it gives the user a choice whether the user wants to continue or 
+exit and secondly, the loop will run atleast once regardless if the condition inside while is true or not*/
 
+#include<stdio.h>
+int main(){
+  int num, sum=0;
+  do {
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num == 0)
+      break;
+    sum = sum + num;
+    printf("Current Sum: %d\n", sum);}
+    while(num != 0);
+}

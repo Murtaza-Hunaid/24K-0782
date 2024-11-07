5b826f10f8238b3feeb2ed4fdbb12e0fefb82340
MurtazaHunaid <k240782@nu.edu.pk>
2024-11-07 23:47:48 +0500

    Create Question 2.c

---
+++ Lab 9/More Problems/Question 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swapIntegers(int a, int b);
+int main(){
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    printf("Before swapping: first number is %d and second number is %d.\n", num1, num2);
+    swapIntegers(num1, num2);
+    return 0;
+}
+
+void swapIntegers(int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    printf("The numbers have been swapped. First number is %d and second number is %d", a, b);
+}

de8a29b34c35adbb80141b3ea39c0a2e635d71ee
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-04-22 15:02:20 +0500

    Create Task 2.cpp

---
+++ Semester 2/OOP Lab/Lab 10/Task 2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string name, email, summary;
+    int yearsOfExperience;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Enter your email: ";
+    getline(cin, email);
+    cout << "Enter your years of experience: ";
+    cin >> yearsOfExperience;
+    cin.ignore();
+    cout << "Enter a brief summary of your experience: ";
+    getline(cin, summary);
+    ofstream outFile("resume.txt", ios::trunc);
+    outFile << "Name: " << name << endl;
+    outFile << "Email: " << email << endl;
+    outFile << "Years of Experience: " << yearsOfExperience << endl;
+    outFile << "Summary: " << summary << endl;
+    outFile.close();
+    ifstream inFile("resume.txt");
+    string line;
+    cout << "\nResume content after update:\n";
+    while (getline(inFile, line)) {
+        cout << line << endl; }
+    inFile.close();
+    return 0;
+}

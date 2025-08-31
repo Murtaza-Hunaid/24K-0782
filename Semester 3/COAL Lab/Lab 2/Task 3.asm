eb9fcd2e4e3e49c451582c6d5b36a1a029256731
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-08-31 23:08:37 +0500

    Create Task 3.asm

---
+++ Semester 3/COAL Lab/Lab 2/Task 3.asm
@@ -0,0 +1,14 @@
+INCLUDE Irvine32.inc
+
+.code
+main PROC
+    mov ebx, 6F1h
+    sub ebx, eax
+    add ebx, 92d
+    add ebx, 47o
+    sub ebx, 11011001b
+    add ebx, 6Ch
+    call WriteInt
+    exit
+main ENDP
+END main

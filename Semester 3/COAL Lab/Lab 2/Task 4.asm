e5d26aac5ae67449433c398ed7ff0b2cff66e427
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-08-31 23:56:07 +0500

    Update Task 4.asm

--- Semester 3/COAL Lab/Lab 2/Task 4.asm
+++ Semester 3/COAL Lab/Lab 2/Task 4.asm
@@ -2,12 +2,15 @@ INCLUDE Irvine32.inc
 
 .code
 main PROC
+    mov ebx, 20
+    mov ecx, 30
+
     mov edx, ecx    ; Saving original ecx to edx
     mov ecx, 101011010110b
     add ecx, 3Ah
     sub ecx, 64o
     add ecx, ebx
-    sub ecx, edx    ; Substracting original ecx(edx) from the new ecx
+    sub ecx, edx    ; Substracting original ecx(edx) from new ecx
     add ecx, 5
     call WriteInt
     exit

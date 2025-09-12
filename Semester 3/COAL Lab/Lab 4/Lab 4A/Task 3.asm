2ced31e1c9e171de5ae98af749308b0d5543a467
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-12 20:18:43 +0500

    Create Task 3.asm

---
+++ Semester 3/COAL Lab/Lab 4/Lab 4A/Task 3.asm
@@ -0,0 +1,21 @@
+; Murtaza Hunaid Terai  24K-0782
+
+INCLUDE Irvine32.inc
+
+.data
+day_temp   DWORD 30
+night_temp DWORD 18
+diff      DWORD ?
+
+.code
+main PROC
+    mov eax, day_temp
+    sub eax, night_temp
+    mov diff, eax
+    mov eax, night_temp
+    add eax, 2
+    mov night_temp, eax
+    call DumpRegs
+    exit
+main ENDP
+END main

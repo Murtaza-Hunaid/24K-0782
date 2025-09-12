a8d0226f5376e5c640d0c86c1d09c1b307c3949b
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-12 19:56:22 +0500

    Create Task 10.asm

---
+++ Semester 3/COAL Lab/Lab 3/Lab 3A/Task 10.asm
@@ -0,0 +1,13 @@
+; Murtaza Hunaid Terai  24K-0782
+
+INCLUDE Irvine32.inc
+
+.data
+msg BYTE " COMPUTER ORGANIZATION & ASSEMBLY LANGUAGE LAB",0
+    BYTE 100 DUP(?)   
+
+.code
+main PROC
+    exit
+main ENDP
+END main

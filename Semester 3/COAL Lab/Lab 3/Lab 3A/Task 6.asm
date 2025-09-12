ed991ee42f8872d451c04e159588a862ca3eb666
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-12 18:29:53 +0500

    Create Task 6.asm

---
+++ Semester 3/COAL Lab/Lab 3/Lab 3A/Task 6.asm
@@ -0,0 +1,15 @@
+; Murtaza Hunaid Terai  24K-0782
+
+INCLUDE Irvine32.inc
+
+.data
+marks WORD 5 DUP(3 DUP(?))
+subject1 BYTE "Math",0
+subject2 BYTE "Programming",0
+subject3 BYTE "Database",0
+
+.code
+main PROC
+    exit
+main ENDP
+END main

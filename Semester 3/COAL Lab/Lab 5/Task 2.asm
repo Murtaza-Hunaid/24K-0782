f48f17bb39cc28136de37b59b02aec0597924cb2
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-17 00:53:58 -0700

    Create Task 2.asm

---
+++ Semester 3/COAL Lab/Lab 5/Task 2.asm
@@ -0,0 +1,13 @@
+; Murtaza Hunaid Terai  24K-0782
+
+INCLUDE Irvine32.inc
+
+.code
+    main proc
+    mov ax, 0A7FF0h 
+    add al, 15h ; a CF = , SF = , ZF = , OF = 
+    add ah, 1h ; b CF =, SF = , ZF = , OF =
+    add ax, 2h ; c CF = , SF = , ZF = , OF =
+    EXIT
+MAIN ENDP
+END main

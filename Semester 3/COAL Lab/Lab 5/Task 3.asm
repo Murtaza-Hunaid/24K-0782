4d5b65e80cf46a615ac8bf42dc35447f2c521dfb
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-26 02:29:22 +0500

    Update Task 3.asm

--- Semester 3/COAL Lab/Lab 5/Task 3.asm
+++ Semester 3/COAL Lab/Lab 5/Task 3.asm
@@ -4,24 +4,20 @@ INCLUDE Irvine32.inc
 
 .data
     array1  WORD 45, 32, 71, 44, 92
-    array2  WORD 32, 44, 45, 71, 92  
-    msg     BYTE "Sorted Array: ",0
+    array2  WORD 32, 44, 45, 71, 92
 
 .code
 main PROC
-    mov edx, OFFSET msg
-    call WriteString
-    call Crlf
-    mov ecx, LENGTHOF array2      
+    mov ecx, LENGTHOF array2
     mov esi, OFFSET array2
 
-looper:
-    movzx eax, WORD PTR [esi]    
-    call WriteDec                
-    mov al, ' '                  
+print_loop:
+    movzx eax, WORD PTR [esi]
+    call WriteDec
+    mov al, ' '
     call WriteChar
-    add esi, TYPE array2          
-    loop looper
+    add esi, TYPE array2
+    loop print_loop
     call Crlf
     exit
 main ENDP

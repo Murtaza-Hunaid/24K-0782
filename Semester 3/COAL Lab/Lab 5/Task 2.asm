347a90d7e0e6653264f0a33aa8bf2c650c889252
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-09-26 17:33:41 +0500

    Update Task 2.asm

--- Semester 3/COAL Lab/Lab 5/Task 2.asm
+++ Semester 3/COAL Lab/Lab 5/Task 2.asm
@@ -1,6 +1,6 @@
 Murtaza Hunaid Terai  24K-0782
 
 mov ax, 0A7FF0h 
-add al, 15h ; a CF = , SF = , ZF = , OF = 
-add ah, 1h ; b CF =, SF = , ZF = , OF =
-add ax, 2h ; c CF = , SF = , ZF = , OF =
+add al, 15h ; a CF = 1, SF = 0, ZF = 0, OF = 0
+add ah, 1h ; b CF = 0, SF = 1, ZF = 0, OF = 0
+add ax, 2h ; c CF = 0, SF = 1, ZF = 0, OF = 0

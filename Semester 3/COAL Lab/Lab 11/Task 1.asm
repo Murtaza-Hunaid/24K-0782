bba588a01c7950b33111011a26b27ca84d7b3a32
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-11-20 15:30:53 +0500

    Update Task 1.asm

--- Semester 3/COAL Lab/Lab 11/Task 1.asm
+++ Semester 3/COAL Lab/Lab 11/Task 1.asm
@@ -3,24 +3,17 @@
 INCLUDE Irvine32.inc
 
 .data
-    Str1 BYTE "127&j~3#^&*#*#45^", 0
-    msg1 BYTE "Found at ", 0
-    msg2 BYTE "Not found", 0
+    Str1 BYTE "127&j~3#^&*#*#45^",0
+    msg1 BYTE "Index = ",0
+    msg2 BYTE "Not found",0
 
 .code
 main PROC
-    mov edi, OFFSET Str1
-    mov al, '#'
-    mov ecx, LENGTHOF Str1
-    cld
-    repne scasb
-    jnz not_found
-
-    sub edi, OFFSET Str1
-    dec edi
+    call Scan_String
+    cmp eax, -1
+    je not_found
     mov edx, OFFSET msg1
     call WriteString
-    mov eax, edi
     call WriteDec
     call Crlf
     exit
@@ -31,4 +24,22 @@ not_found:
     call Crlf
     exit
 main ENDP
+
+Scan_String PROC
+    mov edi, OFFSET Str1
+    mov al, '#'
+    mov ecx, LENGTHOF Str1
+    cld
+    repne scasb
+    jnz notfound
+
+    sub edi, OFFSET Str1
+    mov eax, edi
+    dec eax
+    ret
+notfound:
+    mov eax, -1
+    ret
+Scan_String ENDP
+
 END main

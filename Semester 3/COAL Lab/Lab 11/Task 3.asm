35db7c9c4c6a678032a5c52567909e41d96ce9af
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-11-22 00:51:29 +0500

    Update Task 3.asm

--- Semester 3/COAL Lab/Lab 11/Task 3.asm
+++ Semester 3/COAL Lab/Lab 11/Task 3.asm
@@ -3,68 +3,64 @@
 INCLUDE Irvine32.inc
 
 .data
-    str1 BYTE "Murtaza", 0
-    str2 BYTE "Hunaid", 0
-    a BYTE "str 1 is greater than str 2", 0
-    b BYTE "str 1 is equal to str 2", 0
-    e BYTE "str 1 is less than str 2", 0
+    str1 BYTE "Murtaza",0
+    str2 BYTE "Hunaid",0
+    a BYTE "String 1 is greater than String 2",0
+    b BYTE "String 1 is equal to String 2",0
+    e BYTE "String 1 is less than String 2",0
 
 .code
-main PROC
-    push OFFSET str2
-    push OFFSET str1
-    call IsCompare
-
-    jbe else1
-    mov edx, OFFSET a
-    jmp printmsg
-
-else1:
-    je equalCase
-    mov edx, OFFSET e
-    jmp printmsg
-
-equalCase:
-    mov edx, OFFSET b
-
-printmsg:
-    call WriteString
-    exit
-main ENDP
-
-IsCompare PROC
-    push ebp
-    mov  ebp, esp
-    mov esi, [ebp+8]
-    mov edi, [ebp+12]
-
+IsCompare PROC str1Addr, str2Addr
+    mov esi, str1Addr
+    mov edi, str2Addr
+    
 compare_loop:
     mov al, [esi]
     mov bl, [edi]
     cmp al, bl
-    ja  greater
-    jb  lesser
-
+    ja greater
+    jb lesser
     cmp al, 0
     je equalEnd
-
+    
     inc esi
     inc edi
     jmp compare_loop
-
+    
 greater:
     mov eax, 1
     jmp done
-
+    
 lesser:
     mov eax, -1
     jmp done
-
+    
 equalEnd:
     mov eax, 0
-
+    
 done:
-    pop ebp
-    ret 8
+    ret
 IsCompare ENDP
+
+main PROC
+    INVOKE IsCompare, OFFSET str1, OFFSET str2
+    cmp eax, 1
+    je greaterCase
+    cmp eax, -1
+    je lesserCase
+    mov edx, OFFSET b
+    jmp printmsg
+    
+greaterCase:
+    mov edx, OFFSET a
+    jmp printmsg
+    
+lesserCase:
+    mov edx, OFFSET e
+    
+printmsg:
+    call WriteString
+    call Crlf
+    exit
+main ENDP
 END main

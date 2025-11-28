e45c5d6375751498fefb02692dedeef75dc0a3e2
Murtaza-Hunaid <murtazainhouse@gmail.com>
2025-11-28 10:11:57 +0500

    Create Task 1.asm

---
+++ Semester 3/COAL Lab/Lab 12/Task 1.asm
@@ -0,0 +1,49 @@
+; Murtaza Hunaid Terai  24K-0782
+
+INCLUDE Irvine32.inc
+
+.data
+    str1 BYTE "ASSEMBLY",0       
+    target BYTE 'E' ; Character to search for
+    found DWORD 0   ; 0 = not found, 1 = found
+    msg1 BYTE "Character found!",0
+    msg2 BYTE "Character not found!",0
+
+.code
+main PROC
+    mov esi, OFFSET str1    ; esi points to start of the string
+
+SearchLoop:
+    mov al, BYTE PTR [esi]  ; Loads current character
+    cmp al, 0   ; Check if end of string or not
+    je NotFound ; If null terminator, then exit loop
+
+    cmp al, target  ; Compare with the target character
+    je Found    ; If equal, then go to found
+
+    inc esi ; Move to next character
+    jmp SearchLoop
+
+Found:
+    mov found, 1    ; Set found = 1
+
+NotFound:
+    mov eax, found 
+    cmp eax, 1
+    jne PrintNotFound
+
+PrintFound:
+    mov edx, OFFSET msg1
+    call Writestring
+    call CrLf
+    jmp EndProgram
+
+PrintNotFound:
+    mov edx, OFFSET msg2
+    call Writestring
+    call Crlf
+
+EndProgram:
+    exit
+main ENDP
+END main

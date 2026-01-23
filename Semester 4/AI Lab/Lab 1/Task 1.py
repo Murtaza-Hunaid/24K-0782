b947c47b595cdd26edf0212d44eacffb1dc9133e
Murtaza-Hunaid <murtazainhouse@gmail.com>
2026-01-23 23:27:21 +0500

    Create Task 1.py

---
+++ Semester 4/AI Lab/Lab 1/Task 1.py
@@ -0,0 +1,13 @@
+name = input("Enter student name: ")
+marks = int(input("Enter marks: "))
+if marks >= 85:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 50:
+    grade = "C"
+else:
+    grade = "Fail"
+print("Student Name:", name)
+print("Marks:", marks)
+print("Grade:", grade)

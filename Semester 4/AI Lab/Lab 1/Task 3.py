c8da7ef13e5b53815b4f759d2b67f8dd9c01719b
Murtaza-Hunaid <murtazainhouse@gmail.com>
2026-01-23 23:48:10 +0500

    Create Task 3.py

---
+++ Semester 4/AI Lab/Lab 1/Task 3.py
@@ -0,0 +1,8 @@
+students = {}
+for i in range(3):
+    name = input("Enter student name: ")
+    marks = int(input("Enter marks: "))
+    students[name] = marks
+print("Student Records:")
+for name in students:
+    print(name, ":", students[name])
